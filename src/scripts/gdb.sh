1f963be4702e9b7bd56229cf424816fee116bc3b
Leopold <leommxj@gmail.com>
2020-03-11 00:04:35 +0800

    remove libthread_db support for gdbserver

--- src/scripts/gdb.sh
+++ src/scripts/gdb.sh
@@ -2,6 +2,7 @@ export PATH=`pwd`/buildroot-${BUILDROOT_VERSION}/output/host/bin:$PATH
 wget https://ftp.gnu.org/gnu/gdb/gdb-${GDB_VERSION}.tar.gz
 tar xf gdb-${GDB_VERSION}.tar.gz
 cd gdb-${GDB_VERSION}
+sed -i -e 's/srv_linux_thread_db=yes//' gdb/gdbserver/configure.srv
 ./configure --target=${M} --host=${M} --program-prefix=${M}
 make CFLAGS="-O2 -static" CXXFLAGS="-O2 -static" || echo "FUCK ${M}" # may failed to compile all but successful build gdb/gdbserver
 $M-strip gdb/gdb
