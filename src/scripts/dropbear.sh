9cacbf9b80f65c23c7ff667baf98af4245e9e1de
leommxj <leommxj@gmail.com>
2020-03-26 22:21:17 +0800

    add strace/tcpdump/dropbear(some arch)

--- src/scripts/dropbear.sh
+++ src/scripts/dropbear.sh
@@ -2,10 +2,12 @@ cd buildroot-${BUILDROOT_VERSION}
 export TARGET_A=${TARGET_ARCH}
 unset TARGET_ARCH
 sed -i "s/BR2_SHARED_STATIC_LIBS=y/BR2_STATIC_LIBS=y/g" ./.config
-make socat
+make zlib
+make zlib-ng
+make dropbear
 export PATH=$PATH:`pwd`/output/host/bin
-${M}-strip output/build/socat-*/socat
-ls -al output/build/socat-*/socat
-file output/build/socat-*/socat
-mv output/build/socat-*/socat ../${TARGET_A}/
+${M}-strip output/build/dropbear-*/dropbearmulti
+ls -al output/build/dropbear-*/dropbearmulti
+file output/build/dropbear-*/dropbearmulti
+mv output/build/dropbear-*/dropbearmulti ../${TARGET_A}/
 sed -i "s/BR2_STATIC_LIBS=y/BR2_SHARED_STATIC_LIBS=y/g" ./.config
