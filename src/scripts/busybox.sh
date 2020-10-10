dc009dd2e979f308e7f135978e917afca3d48506
Leopold <leommxj@gmail.com>
2020-10-10 18:08:55 +0800

    update busybox.sh to avoid dynamically linked busybox

--- src/scripts/busybox.sh
+++ src/scripts/busybox.sh
@@ -1,6 +1,7 @@
 source path.sh
 cd buildroot-${BUILDROOT_VERSION}
 sed -i "s/BR2_SHARED_STATIC_LIBS=y/BR2_STATIC_LIBS=y/g" ./.config
+make busybox-dirclean
 make busybox
 ${M}-strip output/build/busybox-*/busybox
 ls -al output/build/busybox-*/busybox
