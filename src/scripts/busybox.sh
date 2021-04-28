ba8d571778858ffcd849aab72beabd38b10cc412
Leopold <leommxj@gmail.com>
2021-04-29 02:02:49 +0800

    update busybox.sh to create busybox_nodrop
    update busybox.sh to build busybox_nodrop which change all BB_SUID_DROP applets to BB_SUID_MAYBE

--- src/scripts/busybox.sh
+++ src/scripts/busybox.sh
@@ -1,10 +1,24 @@
 source path.sh
 cd buildroot-${BUILDROOT_VERSION}
 sed -i "s/BR2_SHARED_STATIC_LIBS=y/BR2_STATIC_LIBS=y/g" ./.config
+# normal busybox
 make busybox-dirclean
 make busybox
 ${M}-strip output/build/busybox-*/busybox
 ls -al output/build/busybox-*/busybox
 file output/build/busybox-*/busybox
 mv output/build/busybox-*/busybox ../${CFG_TARGET_ARCH}/
+# busybox without BB_SUID_DROP
+make busybox-dirclean
+cat << EOF >> package/busybox/busybox.mk
+define BUSYBOX_SWITCH_SUID_HACK
+        grep -e "applet:.*BB_SUID_DROP" -rl \$(@D)/ |xargs sed -i 's/\(applet:.*\)BB_SUID_DROP/\1BB_SUID_MAYBE/g'
+endef
+BUSYBOX_POST_CONFIGURE_HOOKS += BUSYBOX_SWITCH_SUID_HACK
+EOF
+make busybox
+${M}-strip output/build/busybox-*/busybox
+ls -al output/build/busybox-*/busybox
+file output/build/busybox-*/busybox
+mv output/build/busybox-*/busybox ../${CFG_TARGET_ARCH}/busybox_nodrop
 sed -i "s/BR2_STATIC_LIBS=y/BR2_SHARED_STATIC_LIBS=y/g" ./.config
