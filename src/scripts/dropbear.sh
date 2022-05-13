1b205718d2127c6433ca3d611ab9a16bbfc703a6
leommxj <leommxj@gmail.com>
2022-05-13 17:53:34 +0800

    1. static link dropbear, and enable BR2_PACKAGE_DROPBEAR_SMALL to remove zlib and libtomcrypt dep (mips1/mips1el is still dynamic link , maybe because dropbear 2014.66 doesn't care --enable-static)

--- src/scripts/dropbear.sh
+++ src/scripts/dropbear.sh
@@ -2,12 +2,13 @@ source path.sh
 cd buildroot-${BUILDROOT_VERSION}
 export TARGET_A=${CFG_TARGET_ARCH}
 unset TARGET_ARCH
+cp ./.config ./.config.bak
 sed -i "s/BR2_SHARED_STATIC_LIBS=y/BR2_STATIC_LIBS=y/g" ./.config
-make zlib
-make zlib-ng
+echo -ne "\nBR2_PACKAGE_DROPBEAR=y\nBR2_PACKAGE_DROPBEAR_SMALL=y" >> ./.config
+make olddefconfig
 make dropbear
 ${M}-strip output/build/dropbear-*/dropbearmulti
 ls -al output/build/dropbear-*/dropbearmulti
 file output/build/dropbear-*/dropbearmulti
 mv output/build/dropbear-*/dropbearmulti ../${TARGET_A}/
-sed -i "s/BR2_STATIC_LIBS=y/BR2_SHARED_STATIC_LIBS=y/g" ./.config
+mv ./.config.bak ./.config
