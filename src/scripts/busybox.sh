bd5f35dcd9b2b4cf36a46843ed2ec92df8090389
leommxj <leommxj@gmail.com>
2023-03-05 22:20:56 +0800

    enable CONFIG_TELNETD and CONFIG_FEATURE_TELNETD_STANDALONE in busybox

--- src/scripts/busybox.sh
+++ src/scripts/busybox.sh
@@ -3,6 +3,9 @@ cd buildroot-${BUILDROOT_VERSION}
 sed -i "s/BR2_SHARED_STATIC_LIBS=y/BR2_STATIC_LIBS=y/g" ./.config
 # normal busybox
 make busybox-dirclean
+# enable telnetd
+sed -i "s/# CONFIG_TELNETD is not set/CONFIG_TELNETD=y/g" package/busybox/busybox.config
+sed -i "s/# CONFIG_FEATURE_TELNETD_STANDALONE is not set/CONFIG_FEATURE_TELNETD_STANDALONE=y/g" package/busybox/busybox.config
 make busybox
 ${M}-strip output/build/busybox-*/busybox
 ls -al output/build/busybox-*/busybox
@@ -16,6 +19,9 @@ define BUSYBOX_SWITCH_SUID_HACK
 endef
 BUSYBOX_POST_CONFIGURE_HOOKS += BUSYBOX_SWITCH_SUID_HACK
 EOF
+# enable telnetd
+sed -i "s/# CONFIG_TELNETD is not set/CONFIG_TELNETD=y/g" package/busybox/busybox.config
+sed -i "s/# CONFIG_FEATURE_TELNETD_STANDALONE is not set/CONFIG_FEATURE_TELNETD_STANDALONE=y/g" package/busybox/busybox.config
 make busybox
 ${M}-strip output/build/busybox-*/busybox
 ls -al output/build/busybox-*/busybox
